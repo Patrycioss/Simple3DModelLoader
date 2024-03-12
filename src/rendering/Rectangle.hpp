1d86fca46c10467af581d442ec68fd2d02339238
Patrick Schuur <patrickschuurdev@gmail.com>
2024-03-12 17:32:36 +0100

    Rectangle working

---
+++ src/rendering/Rectangle.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../data/Vec2.hpp"
+
+
+class Rectangle 
+{
+private:
+	unsigned int VBO{};
+	unsigned int VAO{};
+	unsigned int EBO{};
+
+public:
+	explicit Rectangle(Vec2<float> topLeft, Vec2<float> bottomLeft, Vec2<float> bottomRight, Vec2<float> topRight);
+	void Draw() const;
+	~Rectangle();
+};
