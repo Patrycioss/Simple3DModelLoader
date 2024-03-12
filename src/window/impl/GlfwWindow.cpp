68822619152c8a564cb5258b85117531d71d62c0
Patrick Schuur <patrickschuurdev@gmail.com>
2024-03-12 16:42:47 +0100

    Remove old shader sources

--- src/window/impl/GlfwWindow.cpp
+++ src/window/impl/GlfwWindow.cpp
@@ -4,20 +4,6 @@
 
 #include "../../ShaderProgram.hpp"
 
-auto vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
-
-auto fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\0";
-
 void processInput(GLFWwindow* window)
 {
 }
